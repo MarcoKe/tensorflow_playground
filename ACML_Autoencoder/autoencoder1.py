943caebf3db437ff9d64f99ba9d0587144ee090c
Marco Kemmerling <marco@kemmerling.me>
2017-09-15 10:57:21 +0200

    added TODO

--- ACML_Autoencoder/autoencoder1.py
+++ ACML_Autoencoder/autoencoder1.py
@@ -1,6 +1,8 @@
 # autoencoder that receives a one-hot string of length 8
 # e.g. [0, 0, 0, 1, 0, 0, 0, 0] with the goal to reproduce
-# that same string as output 
+# that same string as output
+
+# TODO: find a way to access the activations of the the hidden layer
 
 import tensorflow as tf
 import numpy as np
